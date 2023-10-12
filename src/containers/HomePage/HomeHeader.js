19cc4aca9f014430ec6825be7ff82e8fb4d4c52e
dangdung595 <dangdungone@gamil.com>
2023-10-12 10:37:27 +0700

    video58

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,92 +1,148 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
-import logo from '../../assets/logo.svg';
-import { FormattedMessage } from 'react-intl';
+import logo from "../../assets/logo.svg";
+import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils/constant";
 
+import { changeLanguageApp } from "../../store/actions/appActions";
 
 class HomeHeader extends Component {
+
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language)
+  };
   render() {
-    console.log("check",this.props);
+    let language = this.props.language
+    console.log("check", this.props);
     return (
       <React.Fragment>
         <div className="home-header-container">
           <div className="home-header-content">
             <div className="left-content">
-              <i class="fas fa-bars"></i>
+              <i className="fas fa-bars"></i>
               <img className="header-logo" src={logo}></img>
               {/* <div className="header-logo"></div> */}
             </div>
             <div className="center-content">
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.speciality"/> </b>
+                  <b>
+                    <FormattedMessage id="homeheader.speciality" />{" "}
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="homeheader.searchdoctor"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.searchdoctor" />
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.health-facilities"/></b>
+                  <b>
+                    <FormattedMessage id="homeheader.health-facilities" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="homeheader.select-rom"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-rom" />
+                </div>
               </div>
               <div className="child-content">
                 <div>
-                  <b><FormattedMessage id="homeheader.doctor"/></b>
+                  <b>
+                    <FormattedMessage id="homeheader.doctor" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="homeheader.select-doctor"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-doctor" />
+                </div>
               </div>
               <div>
                 <div className="child-content">
-                  <b> <FormattedMessage id="homeheader.fee"/></b>
+                  <b>
+                    {" "}
+                    <FormattedMessage id="homeheader.fee" />
+                  </b>
                 </div>
-                <div className="subs-title"><FormattedMessage id="homeheader.check-health"/></div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.check-health" />
+                </div>
               </div>
             </div>
             <div className="right-content">
               <div className="support">
-              <i class="far fa-question-circle"></i>
-                <FormattedMessage id="homeheader.support"/>
+                <i className="far fa-question-circle"></i>
+                <FormattedMessage id="homeheader.support" />
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
+              <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}>
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VI</span>
+              </div>
+              <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}>
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span>
+              </div>
             </div>
           </div>
         </div>
         <div className="home-header-banner">
           <div className="content-up">
-            <div className="title1"><FormattedMessage id="banner.title1"/></div>
-            <div className="title2"><FormattedMessage id="banner.title2"/></div>
+            <div className="title1">
+              <FormattedMessage id="banner.title1" />
+            </div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2" />
+            </div>
             <div className="search">
-              <i class="fas fa-search"></i>
+              <i className="fas fa-search"></i>
               <input type="text" placeholder="Tim dung dep trai"></input>
             </div>
           </div>
           <div className="content-dow">
             <div className="optines">
               <div className="option-child">
-                <div className="icon-child"><i class="fas fa-hospital-alt"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child1"/></div>
+                <div className="icon-child">
+                  <i className="fas fa-hospital-alt"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1" />
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i class="fas fa-mobile-alt"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child2"/></div>
+                <div className="icon-child">
+                  <i className="fas fa-mobile-alt"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2" />
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i class="fas fa-hospital"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child3"/></div>
+                <div className="icon-child">
+                  <i className="fas fa-hospital"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3" />
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i class="fas fa-thermometer-half"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child4"/></div>
+                <div className="icon-child">
+                  <i className="fas fa-thermometer-half"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4" />
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i class="far fa-user"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child5"/></div>
+                <div className="icon-child">
+                  <i className="far fa-user"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5" />
+                </div>
               </div>
               <div className="option-child">
-                <div className="icon-child"><i class="fas fa-user-md"></i></div>
-                <div className="text-child"><FormattedMessage id="banner.child6"/></div>
+                <div className="icon-child">
+                  <i className="fas fa-user-md"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6" />
+                </div>
               </div>
             </div>
           </div>
@@ -105,7 +161,9 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
